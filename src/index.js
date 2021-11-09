7a9071510bb2d55ef370b52f8bce4ac97718d009
Amrhub <amr_ahmed65@hotmail.com>
2021-11-09 23:53:51 +0200

    feat: add dynamically list items

--- src/index.js
+++ src/index.js
@@ -1,2 +1,29 @@
-// import _ from 'lodash'; will be used later on the project
+import _ from 'lodash';
 import './style.css';
+
+function renderLeaderBoard() {
+  const leaderBoardItems = [
+    {
+      name: 'Nicu',
+      score: 100,
+    },
+    {
+      name: 'Amr',
+      score: 45,
+    },
+    {
+      name: 'Abel',
+      score: 123,
+    },
+  ];
+  const listItemClassName = 'col--leaderboard__list--item';
+  const leaderBoardList = _.map(leaderBoardItems, (item) => {
+    const { name, score } = item;
+    return `<li class="${listItemClassName}">${name}: ${score}</li>`;
+  });
+  const docLeaderBoardList = document.getElementById('leaderboard');
+
+  docLeaderBoardList.innerHTML = leaderBoardList.join('');
+}
+
+renderLeaderBoard();
