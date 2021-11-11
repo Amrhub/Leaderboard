63f01f80b20c20d365f4f3053a4db98faf2e438e
Amrhub <amr_ahmed65@hotmail.com>
2021-11-11 04:19:27 +0200

    feat: made required changes

--- src/leaderboardApi.js
+++ src/leaderboardApi.js
@@ -2,17 +2,19 @@
 const gameId = 'KvmpNbDRknvr72mDOdNz';
 const url = `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${gameId}/scores`;
 
-export async function addUserScore(userScore = {}) {
-  fetch(url, {
+const addUserScore = async (userScore = {}) => {
+  await fetch(url, {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
     body: JSON.stringify(userScore),
   });
-}
+};
 
-export async function getLeaderBoard() {
+const getLeaderBoard = async () => {
   const response = await fetch(url);
   return response.json();
-}
+};
+
+export { addUserScore, getLeaderBoard };
